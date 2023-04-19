f7d25bea2ee189059425fae59bae6f225bc17065
DannyMyles <dannymyles1999@gmail.com>
2023-04-19 15:06:48 +0300

    Settiong up config

--- src/core/models/imageModel.ts
+++ src/core/models/imageModel.ts
@@ -1,9 +1,13 @@
 export interface IImage {
   type: string;
   name: string;
-  userId: number
+  userId: number;
 }
 
 export class ImageModel implements IImage {
-  constructor(public type: string = '', public name: string = '', public userId: number = 0) {}
+  constructor(
+    public type: string = '',
+    public name: string = '',
+    public userId: number = 0,
+  ) {}
 }
